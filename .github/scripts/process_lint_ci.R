19392d44697647935d2065441ffad81182cb0576
etiennebacher <etienne.bacher@protonmail.com>
2025-09-09 23:44:38 +0200

    save script to process CI results

---
+++ .github/scripts/process_lint_ci.R
@@ -0,0 +1,45 @@
+suppressPackageStartupMessages({
+  library(data.table)
+  library(jsonlite)
+  library(poorman)
+})
+
+main_results_json <- jsonlite::read_json(".github/scripts/results_main.json")
+branch_results_json <- jsonlite::read_json(".github/scripts/results_pr.json")
+
+main_results <- lapply(main_results_json, \(x) {
+  data.frame(
+    name = x$message$name,
+    filename = x$filename,
+    row = x$location$row,
+    column = x$location$column
+  )
+}) |>
+  rbindlist()
+
+branch_results <- lapply(branch_results_json, \(x) {
+  data.frame(
+    name = x$message$name,
+    filename = x$filename,
+    row = x$location$row,
+    column = x$location$column
+  )
+}) |>
+  rbindlist()
+
+new_lints <- anti_join(
+  branch_results,
+  main_results,
+  by = c("name", "filename", "row", "column")
+) |>
+  nrow()
+
+deleted_lints <- anti_join(
+  main_results,
+  branch_results,
+  by = c("name", "filename", "row", "column")
+) |>
+  nrow()
+
+paste("**dplyr**:", new_lints, deleted_lints, "violations") |>
+  writeLines("lint_comparison.md")
